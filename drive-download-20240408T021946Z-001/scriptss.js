7a057f28dc21423023386797c1671bfccbe6f825
AngeRubi <165764016+AngeRubi@users.noreply.github.com>
2024-04-11 15:26:40 +0800

    Add files via upload

---
+++ drive-download-20240408T021946Z-001/scriptss.js
@@ -0,0 +1,10 @@
+// variable
+const headerNav  = document.querySelector (".header_nav");
+const toggleMenu = document.querySelector (".toggle_menu");
+
+toggleMenu.addEventListener("click", () =>{
+headerNav.classList.toggle("open");
+toggleMenu.classList.toggle("open");
+
+}
+)
